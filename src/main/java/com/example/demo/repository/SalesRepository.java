27edd942ab1ba5bf3e4059158dab408808ab9b3c
Diego012006 <diego.delcastillo@utec.edu.pe>
2025-11-01 21:38:34 -0500

    applicationProperties

--- src/main/java/com/example/demo/repository/SalesRepository.java
+++ src/main/java/com/example/demo/repository/SalesRepository.java
@@ -14,5 +14,4 @@ public interface SalesRepository extends JpaRepository<Sale, String> {
     @Query("SELECT s FROM Sale s WHERE s.soldAt BETWEEN :from AND :to")
     List<Sale> findByDateRange(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to);
 
-    List<Sale> findByBranch(String branch);
 }
